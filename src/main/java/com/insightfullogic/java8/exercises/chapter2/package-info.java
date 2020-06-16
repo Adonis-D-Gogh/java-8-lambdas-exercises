70149cf12a988bda80aa418d2b508bd7f597623c
Adonis-D-Gogh <Adonis-D-Gogh@github.com>
2020-06-16 09:27:25 +0800

    fix:format

--- src/main/java/com/insightfullogic/java8/exercises/chapter2/package-info.java
+++ src/main/java/com/insightfullogic/java8/exercises/chapter2/package-info.java
@@ -13,4 +13,5 @@ package com.insightfullogic.java8.exercises.chapter2;
  *  a. Yes
  *  b. Yes
  *  c. No - the lambda expression could be inferred as IntPred or Predicate<Integer> so the overload is ambiguous.
+ *  lambda表达式可以推断IntPred或谓语<整数>过载是有歧义的
  */
